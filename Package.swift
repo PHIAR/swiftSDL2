a10a1ad7479e3b117804b2112737267ff58ff7ae
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-07-02 10:58:16 -0700

    Add supported platforms section for Package.swift.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,16 @@
 
 import PackageDescription
 
+// MARK: - Platform configuration
+
+let platforms: [SupportedPlatform] = [
+   .iOS("13.2"),
+   .macOS("10.15"),
+   .tvOS("13.2"),
+]
+
 let package = Package(name: "swiftSDL2",
+                      platforms: platforms,
                       products: [
                           .library(name: "swiftSDL2",
                                    targets: [
